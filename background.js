792476ca5928a6324c18f4964a7a2b4e390725c2
jcconnol <jcconnol4@gmail.com>
2021-11-07 22:57:19 -0600

    adding some nic cage functionality and images

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
-import './nicCage';
-import './mouseSize';
-
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete' && tab.active) {
-        nicCage();
-        mouseSize();
+        runNicCage();
+        //mouseSize();
     }
 });
--- nicCage.js
+++ nicCage.js
@@ -1,8 +1,20 @@
-function nicCage(){
+function runNicCage(){
     chrome.storage.sync.get(["nicCageToggle"], function(items){
-        if(items.nicCageToggle === true){
+        if(items.nicCageToggle == true || items.nicCageToggle == "true"){
             //list out all images and replace them with nic cage images
-            alert("nic cage");
+            var imageArray = document.getElementsByTagName("img");
+
+            var imageNum = 1;
+            for(var i = 0; i < imageArray.length; i++){
+                var fileURL = chrome.runtime.getURL('cageImgs/'+imageNum+".jpg");
+                imageArray[i].src = fileURL;
+
+                imageNum++;
+
+                if(imageNum > 15){
+                    imageNum = 1;
+                }
+            }
         }
     });
 }
